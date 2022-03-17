85df690d6f8d7feec12a7f530eb65568a0449290
smalraju <100932589+smalraju@users.noreply.github.com>
2022-03-17 12:34:50 +0530

    Files added

---
+++ JavaAssignment11/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        File file = new File(args[0]);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        FileWriter writer = new FileWriter("output.txt");
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        String st;
+        while ((st = br.readLine()) != null) {
+            char[] s=st.toCharArray();
+            for (char i:s) {
+
+                if (map.containsKey(i)) {
+                    map.replace(i, map.get(i), map.get(i) + 1);
+                } else {
+                    map.put(i, 1);
+                }
+
+            }
+        }
+        map.remove(' ');
+
+        for (char i : map.keySet()) {
+            writer.write("key: " + i + " value: " + map.get(i));
+            System.out.println("key: " + i + " value: " + map.get(i));
+        }
+        writer.close();
+    }
+}
