2d5ba66e7a83f8725b0d3bd41886d4f74bdab431
smalraju <100932589+smalraju@users.noreply.github.com>
2022-03-19 11:07:25 +0530

    Files Added

---
+++ JavaAssignment12/src/com/company/Student.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    String gender;
+    String engDepartment;
+    int year_of_enrollment;
+    Double perTillDate;
+    Student(int id,String name,int age,String gender,String engDepartment,int year_of_enrollment,Double perTillDate){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.engDepartment=engDepartment;
+        this.year_of_enrollment=year_of_enrollment;
+        this.perTillDate=perTillDate;
+
+    }
+}
